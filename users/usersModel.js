28d81f310d490a37358cfe464e5b400f5f88d234
reber_logan <31495034+reberlvvc@users.noreply.github.com>
2020-01-09 14:06:59 -0600

    All endpoints

--- users/usersModel.js
+++ users/usersModel.js
@@ -4,7 +4,7 @@ const userList = () => {
     return db('users').select('id','username','email');
 }
 const getUser = id => {
-    return db('users').select('username','email').where({id:id}).first();
+    return db('users').select('id','username','email').where({id:id}).first();
 }
 const getGraphs = id => {
     return db('graphs').select('id','name').where({owner:id})
