3bcab862651c3933ec15643a564c57980ffe8f43
Vincent Sanders <vincesanders@hotmail.com>
2020-04-03 11:05:02 -0500

    Added helmet and cors. Fixed some minor typos.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 const server = express();
 const authRouter = require('./auth/authRouter');
 const usersRouter = require('./users/usersRouter');
 const graphsRouter = require('./graphs/graphsRouter');
-server.use(express.json());
+server.use(helmet(), cors(), express.json());
 
 server.use('/api/auth',authRouter);
 server.use('/api/users',usersRouter);
